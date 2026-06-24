516c5658f4e4b1219650d9140ccc417233eb669f
JoseAaronLopezGarcia <joseaaronlopez@gmail.com>
2026-06-24 03:02:00 +0200

    add libpng source code

---
+++ LibPNG/main.c
@@ -0,0 +1,13 @@
+#include <pspsdk.h>
+#include <psploadexec.h>
+
+PSP_MODULE_INFO("LibPNG", PSP_MODULE_SINGLE_LOAD|PSP_MODULE_SINGLE_START, 1, 0);
+
+void _start() __attribute__ ((weak, alias ("module_start")));
+int module_start(){
+    return 0;
+}
+
+void _exit(){
+    sceKernelExitGame();
+}
